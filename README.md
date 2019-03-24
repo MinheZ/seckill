31b89e5d6839c1e91aaa9e42ad737b2c36f70059
MinheZ <1191357474@qq.com>
2019-03-24 19:59:59 +0800

    create db

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # seckill
-实现一个高并发秒杀系统，关键技术：SSM + redis
+
+* [1 秒杀系统业务分析](1-秒杀系统业务分析)
+* [2 开发环境](#2-开发环境)
+* [3 工程创建](#3-工程创建)
+* [4 业务实现](#4-业务实现)
+  * [4.1 数据库建表](#4.1-数据库建表)
+
+-----------------------------
+
+## 1 秒杀系统业务分析
+
+首先分析，秒杀系统问题的本质其实是对商品库存的管理。主要业务逻辑如下图：
+
+<div align="center"><img src="pics//1553423680(1).png" width="500px"></div>
+
+用户针对库存业务分析：
+
+<div align="center"><img src="pics//1553423931(1).png" width="500px"></div>
+
+用户的购买行为：
+
+<div align="center"><img src="pics//1553424057(1).png" width="500px"></div>
+
+**难点**：如何高效地处理“竞争”。
+
+## 2 开发环境
+
+InteliJ IDEA + Maven + Tomcat8 + JDK8
+
+## 3 工程创建
+
+新建一个 Maven 工程，并完善相应的目录结构。
+
+pom 文件的依赖可以分为 4 部分：
+
+- **日志**。使用的是 slf4j + logback 的组合。
+- **数据库**。数据库连接池 + DAO框架，MyBatis依赖。
+- **Servlet web**。jsp 等。
+- **Spring**。主要是 Spring 相关的依赖。
+
+全部依赖参考 [pom.xml](https://github.com/MinheZ/seckill/blob/master/pom.xml)。
+
+## 4 业务实现
+
+### 4.1 数据库建表
+
+- 秒杀库存表
+- 秒杀成功明细表
+
+创建 `seckill`表时，`end_time`字段默认值为`0000-00-00 00:00:00`，报错：1067 - Invalid default value for 'end_time'。
+
+因为在MySQL5.7之后，默认值范围必须为`1970-01-01 10:00:00`~`2037-12-31 23:59:59`。
---
+++ src/main/sql/schema.sql
@@ -0,0 +1,45 @@
+-- 数据库初始化脚本
+
+-- 创建数据库
+CREATE DATABASE seckill;
+-- 使用数据库
+use seckill;
+
+-- 创建秒杀库存表
+CREATE TABLE seckill(
+`seckill_id` bigint NOT NULL AUTO_INCREMENT COMMENT '商品库存id',
+`name` varchar (120) NOT NULL COMMENT '商品名称',
+`number` int NOT NULL COMMENT '库存数量',
+`start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '秒杀开始时间',
+`end_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '秒杀结束时间',
+`create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+-- 设置索引
+PRIMARY KEY (seckill_id),
+KEY idx_start_time(start_time),
+KEY idx_end_time(end_time),
+KEY idx_create_time(create_time)
+)ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='秒杀库存表';
+
+------------------------------------------------------------------------------
+
+-- 初始化数据
+insert into seckill(name, number, start_time, end_time)
+values
+  ('1000元秒杀iPhone X', 100, '2019-3-24 00:00:00', '2019-3-24 01:00:00'),
+  ('500元秒杀小米 9', 100, '2019-3-24 00:00:00', '2019-3-24 01:00:00'),
+  ('3000元秒杀 2080 Ti', 100, '2019-3-24 00:00:00', '2019-3-24 01:00:00'),
+  ('2000元秒杀 6D Mart II', 100, '2019-3-24 00:00:00', '2019-3-24 01:00:00'),
+
+
+-- 秒杀成功明细表
+-- 用户登录认证相关信息
+CREATE TABLE success_killed(
+`seckill_id` bigint NOT NULL '秒杀商品id',
+`user_phone` bigint NOT NULL COMMENT '用户手机号',
+`state` tinyint NOT NULL DEFAULT -1 COMMENT '状态标识:-1:无效；0：成功；1：已付款',
+`create_time` timestamp NOT NULL COMMENT '创建时间',
+PRIMARY KEY(seckill_id, user_phone),  /*联合主键，防止一个用户对同一个产品做秒杀*/
+KEY idx_create_time(create_time)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='秒杀成功明细表';
+
+
